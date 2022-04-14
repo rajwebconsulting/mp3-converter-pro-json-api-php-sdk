cc8b7cbf9d4153d332ce9c3e08d9bb315bafe6f5
rustynail1984 <65692019+rustynail1984@users.noreply.github.com>
2022-04-14 18:19:41 +0200

    Update Config.php

--- test/Config.php
+++ test/Config.php
@@ -5,5 +5,5 @@ namespace Rajwebconsulting;
 class Config
 {
     // MP3 Converter Pro base url
-    const API_URL = 'http://mcp.dev-cloud.mp3youtu.be';
+    const API_URL = 'http://YOUR_MP3_CONVERTER_PRO_URL';
 }
